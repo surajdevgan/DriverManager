ee41ea34707227df41ee48e1aadcd9d03926b610
surajdevgan <sdevgan007@gmail.com>
2018-09-11 18:57:26 +0530

    abcd

--- app/src/main/java/drivermng/app/com/drivermanager/driver/DriverFragment.java
+++ app/src/main/java/drivermng/app/com/drivermanager/driver/DriverFragment.java
@@ -1,11 +1,18 @@
 package drivermng.app.com.drivermanager.driver;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import drivermng.app.com.drivermanager.R;
 
@@ -13,6 +20,8 @@ import drivermng.app.com.drivermanager.R;
  * A simple {@link Fragment} subclass.
  */
 public class DriverFragment extends Fragment {
+    EditText edittext;
+    Calendar myCalendar = Calendar.getInstance();
 
 
     public DriverFragment() {
@@ -24,7 +33,38 @@ public class DriverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver, container, false);
+        View view = inflater.inflate(R.layout.fragment_driver, container, false);
+        edittext= view.findViewById(R.id.Birthday);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        return view;
+    }
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
